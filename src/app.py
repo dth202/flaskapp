9738d86fbcf5507cb990169e9aed3c3eb157ddd2
Dallas Harris <dth202@gmail.com>
2020-08-14 06:56:51 -0600

    feat(app.py): add html to simple app

--- src/app.py
+++ src/app.py
@@ -2,11 +2,15 @@
 
 #  loads the Flask framework so that you can use it
 #
-from flask import Flask
+from flask import Flask, render_template
 
 # creates a Flask application to run your code
 #
 app = Flask(__name__)
+
+# Set to True if troubleshooting
+#
+app.config["DEBUG"] = True
 
 # This decorator specifies that the following function defines what happens when someone goes to the location "/" on your site
 #
@@ -15,6 +19,6 @@ app = Flask(__name__)
 
 # This simple function just says that when someone goes to the location, they get back the (unformatted) text "Hello from Flask."
 #
-def hello_world():
-    return 'Hello from Flask!'
+def index():
+    return render_template("main_page.html")
 
